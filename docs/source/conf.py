124333cdcdf977f048f0d8f579eb6f12fae5e3c0
Keshav Priyadarshi <git@keshav.space>
2022-09-08 23:52:11 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SpaceTime'
+copyright = '2022, SpaceTime'
+author = 'Keshav Priyadarshi'
 
 release = '0.1'
 version = '0.1.0'
